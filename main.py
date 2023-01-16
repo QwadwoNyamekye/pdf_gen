4af64b76b9f8f6d5378d317a9d5316ea89d68648
David Oppong-Nkrumah <oppongnkrumahdavid@gmail.com>
2023-01-16 22:01:09 +0000

    flask

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-import os
+from flask import Flask, render_template, url_for
+from flask_weasyprint import HTML, render_pdf
 
-os.add_dll_directory(r"C:\Program Files\GTK3-Runtime Win64\bin")
+app = Flask(__name__)
 
-from weasyprint import HTML
+@app.route('/hello/', defaults={'name': 'World'})
+@app.route('/hello/<name>/')
+def hello_html(name):
+    return render_template('hello.html', name=name)
 
-def makepdf():
-    """Generate a PDF file from a string of HTML."""
-    htmldoc = HTML(filename="templates/mytemplate.html")
-    return htmldoc.write_pdf("ttesstt.pdf")
+@app.route('/hello_<name>.pdf')
+def hello_pdf(name):
+    # Make a PDF from another view
+    return render_pdf(url_for('hello_html', name=name))
 
-makepdf()
+# Alternatively, if the PDF does not have a matching HTML page:
+
+@app.route('/hello_<name>.pdf')
+def hello_pdf(name):
+    # Make a PDF straight from HTML in a string.
+    html = render_template('hello.html', name=name)
+    return render_pdf(HTML(string=html))
