ac4346bd555fde989150810d484323d4381c9c70
david oppong-nkrumah <doppong-nkrumah@itconsortiumgh.com>
2023-01-17 08:46:07 +0000

    added things

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 from flask_weasyprint import HTML, render_pdf
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/generate_pdf', methods=['POST'])
