e8c528d9131be3bb0921a96890f7bb84b736c113
David Oppong-Nkrumah <oppongnkrumahdavid@gmail.com>
2023-01-17 01:05:34 +0000

    final

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 from flask_weasyprint import HTML, render_pdf
 
 app = Flask(__name__)
 
-@app.route('/hello/', defaults={'name': 'World'})
-@app.route('/hello/<name>/')
-def hello_html(name):
-    return render_template('hello.html', name=name)
 
-@app.route('/hello_<name>.pdf')
-def hello_pdf(name):
-    # Make a PDF from another view
-    return render_pdf(url_for('hello_html', name=name))
+@app.route('/generate_pdf', methods=['POST'])
+def generate_pdf():
+    data = request.json
+    student_id = data.get('student_id', '')
+    form = data.get('form', '')
+    name = data.get('name', '')
+    email = data.get('email', '')
+    contact = data.get('contact', '')
+    amount = data.get('amount', '')
 
-# Alternatively, if the PDF does not have a matching HTML page:
-
-@app.route('/hello_<name>.pdf')
-def hello_pdfs(name):
     # Make a PDF straight from HTML in a string.
-    html = render_template('hello.html', name=name)
+    html = render_template('index.html', amount=amount, student_id=student_id, form=form, name=name, email=email,
+                           contact=contact)
     return render_pdf(HTML(string=html))
